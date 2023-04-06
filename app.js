b8fef81a7793cc42c25688e6319ff910ae3676a1
Gustavo Ferreira <gvasconcelos220@gmail.com>
2023-04-06 13:54:47 -0300

    conferencia de dados

--- app.js
+++ app.js
@@ -149,110 +149,7 @@ app.get('/aplicacao', /*validarFormulario,*/ (req, res) => {
                 <input type="text" class="form-control mb-3 w-50" name="numVisaNonImmigrate" id="numVisaNonImmigrate" maxlength="35" required>
 
                 <label class="mb-2" for="dateVisaNonImmigrate">Data de expiração do visto americano de não-imigrante <span class="text-red">* (obrigatório)</span></label>
-                <div class="row">
-                    <div class="col">
-                        <select class="form-select mb-3" name="dayVisaNonImmigrate" id="dayVisaNonImmigrate">
-                            <option selected disabled="disabled">Selecione o dia</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                            <option value="11">11</option>
-                            <option value="12">12</option>
-                            <option value="13">13</option>
-                            <option value="14">14</option>
-                            <option value="15">15</option>
-                            <option value="16">16</option>
-                            <option value="17">17</option>
-                            <option value="18">18</option>
-                            <option value="19">19</option>
-                            <option value="20">20</option>
-                            <option value="21">21</option>
-                            <option value="22">22</option>
-                            <option value="23">23</option>
-                            <option value="24">24</option>
-                            <option value="25">25</option>
-                            <option value="26">26</option>
-                            <option value="27">27</option>
-                            <option value="28">28</option>
-                            <option value="29">29</option>
-                            <option value="30">30</option>
-                            <option value="31">31</option>
-                        </select>
-                    </div>
-                    <div class="col">
-                        <select class="form-select mb-3" name="monthVisaNonImmigrate" id="monthVisaNonImmigrate">
-                            <option selected disabled="disabled">Selecione o mês</option>
-                            <option value="1">Janeiro</option>
-                            <option value="2">Fevereiro</option>
-                            <option value="3">Março</option>
-                            <option value="4">Abril</option>
-                            <option value="5">Maio</option>
-                            <option value="6">Junho</option>
-                            <option value="7">Julho</option>
-                            <option value="8">Agosto</option>
-                            <option value="9">Setembro</option>
-                            <option value="10">Outubro</option>
-                            <option value="11">Novembro</option>
-                            <option value="12">Dezembro</option>
-                        </select>
-                    </div>
-                    <div class="col">
-                        <select class="form-select mb-3" name="yearVisaNonImmigrate" id="yearVisaNonImmigrate">
-                            <option selected disabled="disabled">Selecione o ano</option>
-                            <option value="2023">2023</option>
-                            <option value="2024">2024</option>
-                            <option value="2025">2025</option>
-                            <option value="2026">2026</option>
-                            <option value="2027">2027</option>
-                            <option value="2028">2028</option>
-                            <option value="2029">2029</option>
-                            <option value="2030">2030</option>
-                            <option value="2031">2031</option>
-                            <option value="2032">2032</option>
-                            <option value="2033">2033</option>
-                            <option value="2034">2034</option>
-                            <option value="2035">2035</option>
-                            <option value="2036">2036</option>
-                            <option value="2037">2037</option>
-                            <option value="2038">2038</option>
-                            <option value="2039">2039</option>
-                            <option value="2040">2040</option>
-                            <option value="2041">2041</option>
-                            <option value="2042">2042</option>
-                            <option value="2043">2043</option>
-                            <option value="2044">2044</option>
-                            <option value="2045">2045</option>
-                            <option value="2046">2046</option>
-                            <option value="2047">2047</option>
-                            <option value="2048">2048</option>
-                            <option value="2049">2049</option>
-                            <option value="2050">2050</option>
-                            <option value="2051">2051</option>
-                            <option value="2052">2052</option>
-                            <option value="2053">2053</option>
-                            <option value="2054">2054</option>
-                            <option value="2055">2055</option>
-                            <option value="2056">2056</option>
-                            <option value="2057">2057</option>
-                            <option value="2058">2058</option>
-                            <option value="2059">2059</option>
-                            <option value="2060">2060</option>
-                            <option value="2061">2061</option>
-                            <option value="2062">2062</option>
-                            <option value="2063">2063</option>
-                            <option value="2064">2064</option>
-                            <option value="2065">2065</option>
-                            <option value="2066">2066</option>
-                        </select>
-                    </div>
-                </div>
+                <input type="date" class="form-control mb-3 w-25" name="dateVisaNonImmigrate" id="dateVisaNonImmigrate" onchange="validNotPresentDay(this)" required>   
             `
             res.render('aplicacao-step3', {title, dynamicData})
          } else {
@@ -294,4 +191,3 @@ app.use('/checkout', checkout)
 app.listen(3000, ()=> {
     console.log("SERVER ON!")
 })
-
