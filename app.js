e09fa8791ec26cc39282ac10239eb87910954af7
Gustavo Ferreira <gvasconcelos220@gmail.com>
2023-04-09 21:37:30 -0300

    api mercado pago funcionando

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ app.use((req, res, next) => {
 
 //Mercaado Pago
     mercadopago.configure({
-        sandbox: true,
         access_token: 'TEST-7703581273948303-040210-09008d0ef878c5f0c346329e85b0ac55-718885874',
+        sandbox: true,
     })
 
 
--- routes/checkout.js
+++ routes/checkout.js
@@ -2,67 +2,78 @@ const express = require('express')
 const router = express.Router()
 const mercadopago = require('mercadopago')
 const dotenv = require('dotenv')
-
 dotenv.config()
 
+const mercadoPagoPublicKey = process.env.MERCADO_PAGO_SAMPLE_PUBLIC_KEY;
+if (!mercadoPagoPublicKey) {
+  console.log("Error: public key not defined");
+  process.exit(1);
+}
+
+const mercadoPagoAccessToken = process.env.MERCADO_PAGO_SAMPLE_ACCESS_TOKEN;
+if (!mercadoPagoAccessToken) {
+  console.log("Error: access token not defined")
+  process.exit(1)
+}
+
+mercadopago.configurations.setAccessToken(mercadoPagoAccessToken)
+
 mercadopago.configure({
-  access_token: 'TEST-7703581273948303-040210-09008d0ef878c5f0c346329e85b0ac55-718885874',
+  access_token: process.env.MERCADO_PAGO_SAMPLE_ACCESS_TOKEN,
   sandbox: true
 })
 
 router.get('/', (req, res) => {
-    res.render('checkout/index')
+  const title = 'Checkout - '
+  res.status(200).render('checkout/index', {title, mercadoPagoAccessToken})
 })
 
-router.post('/create-payment', async (req, res) => {
-  const preference = {
-    items: [
-      {
-        title: 'Solicitação de Viagem Eletrônica - Canadá',
-        unit_price: 99.00,
-        quantity: 1
+router.post('/process_payment', (req, res) => {
+  const { body } = req
+  const { payer } = body
+  const paymentData = {
+    transaction_amount: 99.00,
+    token: body.token,
+    description: 'Solicitação de Autorização de Viagem - Canadá',
+    installments: Number(body.installments),
+    payment_method_id: body.paymentMethodId,
+    issuer_id: body.issuerId,
+    payer: {
+      email: payer.email,
+      identification: {
+        type: payer.identification.docType,
+        number: payer.identification.docNumber
       }
-    ],
-    back_urls: {
-      success: 'http://localhost:3000/checkout/success',
-      pending: 'http://localhost:3000/checkout/pending',
-      failure: 'http://localhost:3000/checkout/failure'
-    },
-    auto_return: 'approved',
-    payment_methods: {
-      excluded_payment_types: [
-        { id: 'ticket' },
-        { id: 'atm' }
-      ]
-    },
-    notification_url: 'http://localhost:3000/checkout/notification'
+    }
   }
 
-  try {
-    const response = await mercadopago.preferences.create(preference)
-    const { init_point } = response.body
-    res.redirect(init_point)
-  } catch (error) {
+  mercadopago.payment.save(paymentData).then((response) => {
+    const { response: data } = response
+    res.status(201).json({
+      detail: data.status_detail,
+      status: data.status,
+      id: data.id
+    })
+  }).catch((error) => {
     console.log(error)
-    res.redirect('/checkout')
+    const { errorMessage, errorStatus }  = validateError(error)
+    res.status(errorStatus).json({ error_message: errorMessage })
+  })
+
+  function validateError(error) {
+    let errorMessage = 'Unknown error cause'
+    let errorStatus = 400
+  
+    if(error.cause) {
+      const sdkErrorMessage = error.cause[0].description
+      errorMessage = sdkErrorMessage || errorMessage
+  
+      const sdkErrorStatus = error.status
+      errorStatus = sdkErrorStatus || errorStatus
+    }
+  
+    return { errorMessage, errorStatus }
   }
-})
-
-router.get('/success', (req, res) => {
-  res.render('checkout/success')
-})
-
-router.get('/pending', (req, res) => {
-  res.render('checkout/pending')
-})
-
-router.get('/failure', (req, res) => {
-  res.render('checkout/failure')
-})
-
-router.post('/notification', (req, res) => {
-  console.log(req.body)
-  res.status(200).send('OK')
 })
 
 module.exports = router
